e3745a9fb8ad308429e79da61f0463211f07c9a7
Jessica Ramirez <jessicaramirez330@gmail.com>
2018-07-17 18:35:56 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,16 @@
-require_relative 'config/environment'
+require 'sinatra'
 
 class App < Sinatra::Base
+  get '/' do 
+    "Hello World"
+  end 
+  get '/name' do 
+    "My name is Jessica"
+  end 
+  get '/hometown' do
+    "My hometown is Stockton"
+  end 
+  get '/favorite-song' do
+    "My favorite song is lol"
+  end 
 end
